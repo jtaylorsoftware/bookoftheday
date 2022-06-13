f55333e8d9f4694f1bf939db48a6feab9ae5b884
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2022-06-13 13:23:20 -0700

    Set up SAM template
    - Completed template for initial design spec
    - Template successfully creates stack
    - Lambda function code not yet completed or tested

---
+++ handlers/refresh-lists/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	// "errors"
+	// "fmt"
+
+	// "github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler() error {
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
